955cd295573cc47d12ba6dedaf3e476467f8c4e7
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-16 19:10:27 -0300

    estudo

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, FlatList } from 'react-native';
-
-import Pessoas from  './src/Pessoas/Pessoas' 
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-community/picker'
 
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      feed: [
-        { nome: 'FRANCISCO', idade: 50, email: 'franciscos@gmail.' },
-        { nome: 'Roberto', idade: 32, email: 'roberto@gmail.' },
-        { nome: 'Luara', idade: 15, email: 'luaras@gmail.' },
-        { nome: 'Leopoldo', idade: 20, email: 'leopoldos@gmail.' },
-
+      pizza: 0,
+      pizzas: [
+        { key: 1, nome: "Arroz", valor: 12.00 },
+        { key: 2, nome: "Batata", valor: 32.00 },
+        { key: 3, nome: "Frango", valor: 23.00 },
       ]
-
     }
   }
-
-
   render() {
+    let pizzasItem = this.state.pizzas.map((v, k) => {
+      return <Picker.Item key={k} value={k} label={v.nome} />
+    })
     return (
       <View style={styles.container}>
 
-        <FlatList data={this.state.feed} renderItem={({ item }) => <Pessoas data={item} />} />
+        <Text style={styles.logo}>Menu pizzas</Text>
+
+        <Picker selectedValue={this.state.pizza} onValueChange={(itemValue, itemIndex) => this.setState({ pizza: itemValue })}>
+          {pizzasItem}
+        </Picker>
+
+        <Text style={styles.pizzas}> Você escolheu: {this.state.pizzas[this.state.pizza].nome} </Text>
+
+        <Text style={styles.pizzas}> R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)} </Text>
 
       </View>
     );
@@ -36,8 +42,19 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50
+  },
+  logo: {
+    textAlign: 'center',
+    fontSize: 25,
 
   },
-  
+  pizzas:{
+    marginTop:15,
+    backgroundColor: '#DDD',
+    fontSize: 25,
+    textAlign: 'center'
+  }
+
 
 });
