b8377a4c961d727958bf6108aa9d3a3b21a458fd
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-22 19:51:34 -0300

    inteporlate

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableOpacity } from 'react-native';
 
-import Conversor from './src/Conversor'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      larAnima: new Animated.Value(0)
     };
+
+    Animated.timing(
+      this.state.larAnima, {
+      toValue: 100,
+      duration: 5000,
+      useNativeDriver: false
+    }
+    ).start()
   }
 
+
+
   render() {
-      return (
-        <View style={styles.container}>
-            <Conversor moedaA= 'USD' moedaB='BRL' />
-        </View>
-      );
-    }
+    let porcent = this.state.larAnima.interpolate({
+      inputRange:[0, 100],
+      outputRange: ['0%', '100%']
+    })
+
+    return (
+      <View style={styles.container}>
+
+        <Animated.View style={{ backgroundColor: '#4169E1', width: porcent, height: 25 }}>
+
+        </Animated.View>
+
+      </View>
+    );
   }
+}
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'flex-start'
   },
 });
