4913c28b339d6de94c420232da1c825c1aeec180
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-21 21:38:10 -0300

    conversor de moedas

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
+import axios from 'axios'
+
+
+// https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=ultra&apiKey=788347d95c514e16ee87
 
 const api = axios.create({
-  baseURL: 'https://sujeitoprogramador.com',
-});
+    baseURL: 'https://free.currconv.com/api/v7'
+})
 
-export default api;
+export default api
