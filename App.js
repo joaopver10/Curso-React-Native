4af1f0effcce4871835ec90f15b48935fb47d658
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-28 15:16:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet, View } from 'react-native'
 import firebase from './src/firebasecConnection'
 
 
+console.disableYellowBox = true
 
 export default function App() {
+  const [nome, SetNome] = useState('Carregando...')
+  const [ idade, setIdade] = useState('')
+
+  useEffect(() => {
+
+    async function dados() {
+
+        await firebase.database().ref('usuarios/1').on("value", (snapshot) =>{
+           SetNome(snapshot.val().nome)
+           setIdade(snapshot.val().idade)
+         }) 
+        }
+    /* 
+      await firebase.database().ref('nome').once("value", (snapshot) => {
+        SetNome(snapshot.val())
+      })
+    }
+    */
+
+    dados()
+
+  }, [])
+
   return (
-    <View  style={{marginTop: 55}}>
-      <Text> olá </Text>
+    <View style={{ marginTop: 55 }}>
+      <Text style={{ fontSize: 25 }}> olá {nome} </Text>
+      <Text style={{ fontSize: 25 }}> Idade: {idade} </Text>
     </View>
   )
 }
