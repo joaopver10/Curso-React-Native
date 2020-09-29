022b095f88ef2997da06d6ae749426901b1c7d8d
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-28 22:30:26 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,101 @@
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native'
+import { set } from 'react-native-reanimated';
 import firebase from './src/firebasecConnection'
 
 
 console.disableYellowBox = true
 
 export default function App() {
-  const [nome, SetNome] = useState('Carregando...')
-  const [ idade, setIdade] = useState('')
+  const [nome, setNome] = useState('')
+  const [cargo, setCargo] = useState('')
 
   useEffect(() => {
 
     async function dados() {
 
-        await firebase.database().ref('usuarios/1').on("value", (snapshot) =>{
-           SetNome(snapshot.val().nome)
-           setIdade(snapshot.val().idade)
-         }) 
-        }
-    /* 
-      await firebase.database().ref('nome').once("value", (snapshot) => {
-        SetNome(snapshot.val())
+      //cria um nó
+      // await firebase.database().ref('Tipo').set('Cliente')
+
+      //remove (nó)
+      //await firebase.database().ref('Tipo').remove()
+
+      /*
+      criar um outro child
+      await firebase.database().ref('usuarios').child(2).set({
+        nome: 'jose',
+        cargo: 'Programador'
       })
+      */
+      //atualizar child
+      //await firebase.database().ref('usuarios').child(2).update({
+      // nome: 'Alexandre'
+      // })
+
+
+
+
+
     }
-    */
+
 
     dados()
 
   }, [])
 
+  async function cadastrar(){
+    if (nome !== '' & cargo !== ''){
+      let usuarios = await firebase.database().ref('usuarios')
+      let chave = usuarios.push().key
+
+      usuarios.child(chave).set({
+        nome: nome,
+        cargo: cargo
+      })
+      alert ('cadastrado com sucesso') 
+      setCargo('')
+      setNome('')
+    }
+  }
+
   return (
-    <View style={{ marginTop: 55 }}>
-      <Text style={{ fontSize: 25 }}> olá {nome} </Text>
-      <Text style={{ fontSize: 25 }}> Idade: {idade} </Text>
+    <View style={styles.container}>
+      <Text style={styles.texto}>Nome</Text>
+      <TextInput style={styles.input}
+        underlineColorAndroid='transparent'
+        onChangeText={(texto) => setNome(texto)}
+        value={nome}
+      />
+       <Text style={styles.texto}>Cargo</Text>
+      <TextInput style={styles.input}
+        underlineColorAndroid='transparent'
+        onChangeText={(texto) => setCargo(texto)}
+        value={cargo}
+      />
+      <Button title='Novo funcionario' onPress={cadastrar}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 50
+  },
+  button:{
+
+  },
+  texto:{
+    fontSize: 20
+  },
+  input:{
+    marginBottom: 10,
+    padding:10,
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 45,
+    fontSize: 17
+  }
+
+
+})
