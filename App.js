de0991f497fc5861f58e2eb7d83d5948763eb400
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-09-30 18:43:23 -0300

    relacionando banco de dados

--- App.js
+++ App.js
@@ -1,23 +1,84 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, TextInput, Button, } from 'react-native'
 
 
-import Home from './src/Pages/Home'
-import Acesso from './src/Pages/Acesso'
+import firebase from './src/firebasecConnection'
 
 
 console.disableYellowBox = true
 
-const Stack = createStackNavigator()
-
 export default function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [nome, setNome] = useState('')
+
+  async function cadastrar() {
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((value) => {
+        //alert(value.user.uid)
+        firebase.database().ref('usuarios').child(value.user.uid).set({
+          nome: nome
+        })
+        alert('Usuario criado com sucesso')
+        setNome('')
+        setEmail('')
+        setPassword('')
+      })
+      .catch((error) => {
+        alert('Algo deu errado ')
+      })
+  }
+
+
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Acesso}>
-        <Stack.Screen name='Acesso' component={Acesso} options={{ headerShown: false }} />
-        <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+
+      <Text style={styles.texto}>Nome:</Text>
+      <TextInput style={styles.input}
+        underlineColorAndroid='transparent'
+        onChangeText={(nome) => setNome(nome)}
+        value={nome}
+      />
+
+      <Text style={styles.texto}>Email:</Text>
+      <TextInput style={styles.input}
+        underlineColorAndroid='transparent'
+        onChangeText={(texto) => setEmail(texto)}
+        value={email}
+      />
+      <Text style={styles.texto}>Senha:</Text>
+      <TextInput style={styles.input}
+        underlineColorAndroid='transparent'
+        onChangeText={(texto) => setPassword(texto)}
+        value={password}
+      />
+      <Button title='Cadastrar' onPress={cadastrar} />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    justifyContent: 'center'
+  },
+  button: {
+
+  },
+  texto: {
+    fontSize: 20
+  },
+  input: {
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    height: 45,
+    fontSize: 17
+  }
+
+
+})
