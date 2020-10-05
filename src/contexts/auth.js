d9df5e9029ef7442789db445c2d25935bb0b6056
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-10-05 17:20:36 -0300

    async

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,11 +1,45 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import firebase from '../services/firebasecConnection'
-
+import AsyncStorage from '@react-native-community/async-storage'
 export const AuthContext = createContext({})
 
 export default function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
+    const [load, setLoad] = useState(true)
 
+
+    useEffect( ()=> {
+        async function loadStorage(){
+            const storageUser = await AsyncStorage.getItem('Auth_user')
+
+            if(storageUser){
+                setUser(JSON.parse(storageUser))
+                setLoad(false)
+            }
+            setLoad(false)
+        }
+        loadStorage()
+    },[])
+    //logar 
+    async function usuarioEntrando(email, senha) {
+        await firebase.auth().signInWithEmailAndPassword(email, senha)
+        .then(async(value) => { 
+            let uid = value.user.uid
+            await firebase.database().ref('usuarios').child(uid).once('value')
+            .then((snapshot) =>{
+                let data ={
+                    uid: uid,
+                    nome: snapshot.val().nome,
+                    email: value.user.email,
+                }
+                setUser(data)
+                storageUser(data)
+            })
+        })
+            .catch((error) => {
+                alert(error.code)
+            })
+    }
 
     //cadastrando
     async function cadUsuario(email, senha, nome) {
@@ -23,12 +57,20 @@ export default function AuthProvider({ children }) {
                             email: value.user.email
                         }
                         setUser(data)
+                        storageUser(data)
                     })
+            })
+            .catch((error) => {
+                alert(error.code)
             })
     }
 
+    async function storageUser(data){
+        await AsyncStorage.setItem('Auth_user', JSON.stringify(data))
+    }
+
     return (
-        <AuthContext.Provider value={{ signed: !!user, user, cadUsuario }}>
+        <AuthContext.Provider value={{ signed: !!user, user, cadUsuario, usuarioEntrando, load}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,47 +1,52 @@
-import React, {useState, useContext}from 'react'
+import React, { useState, useContext } from 'react'
 import { View, Text, Platform } from 'react-native'
-import {Background, Container, Logo, AreaInput, Input, SubmitButton, SubmitText, Link, LinkText} from './styles'
-import {useNavigation} from '@react-navigation/native'
-import AuthContext from  '../../contexts/auth'
+import { Background, Container, Logo, AreaInput, Input, SubmitButton, SubmitText, Link, LinkText } from './styles'
+import { useNavigation } from '@react-navigation/native'
+import {AuthContext} from '../../contexts/auth'
 
 export default function SignIn() {
-
     const navigation = useNavigation()
-    const[email, setEmail] = useState('')
-    const[senha, setSenha] = useState('')
 
-    //const {user} = useContext(AuthContext)
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const {usuarioEntrando} = useContext(AuthContext)
+
+    function entrar(){
+        usuarioEntrando(email, senha)
+    }
 
     return (
         <Background>
             <Container behavior={Platform.OS === 'ios' ? 'padding' : ''}
-            enabled
+                enabled
             >
-                <Logo source={require('../../assets/Logo.png')}/>
+                <Logo source={require('../../assets/Logo.png')} />
                 <AreaInput>
                     <Input placeholder='Email'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                    value={email}
-                    onChange={ ( text) => setEmail(text)}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
                     />
                 </AreaInput>
 
                 <AreaInput>
                     <Input placeholder='Senha'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                    value={senha}
-                    onChange={ ( text) => setSenha(text)}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        value={senha}
+                        onChangeText={(text) => setSenha(text)}
                     />
                 </AreaInput>
 
-                <SubmitButton>
+                <SubmitButton onPress={entrar}>
                     <SubmitText> Acessar </SubmitText>
                 </SubmitButton>
 
-                <Link onPress={ () => navigation.navigate('Cadastro')}>
-                <LinkText>Criar uma conta</LinkText>
+                <Link onPress={() => navigation.navigate('Cadastro')}>
+                    <LinkText>Criar uma conta</LinkText>
                 </Link>
 
             </Container>
