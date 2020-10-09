dd02d9cd41ef53f4e76fe85d0ea7796149eaa8ec
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-10-08 21:39:37 -0300

    Home atualizada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,32 @@
-import React, {useContext} from 'react'
-import { View, Text, Button } from 'react-native'
-import {AuthContext} from '../../contexts/auth'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../../contexts/auth'
+import Header from '../../components/Header'
+import { Background, Container, Nome, Saldo, Title, List } from './styles'
+import HistoricoList from '../../components/HistoricoList'
 
-export default function Home(){
-    const {user, deslogando} = useContext(AuthContext)
-    return(
-        <View>
-            <Text>Home </Text>
-            <Button title='sair da conta' onPress={ () => deslogando()}/>
-        </View>
+export default function Home() {
+    const { user } = useContext(AuthContext)
+    const [ historico, setHistorico ] = useState([
+        { key: '1', tipo: 'receita', valor: 1200 },
+        { key: '2', tipo: 'despesa', valor: 1220 },
+        { key: '3', tipo: 'despesa', valor: 1400 },
+        { key: '4', tipo: 'despesa', valor: 130 },
+        { key: '5', tipo: 'receita', valor: 150 },
+    ])
+    return (
+        <Background>
+            <Header />
+            <Container>
+                <Nome>Joao</Nome>
+                <Saldo>123,00</Saldo>
+            </Container>
+            <Title>Ultimas movimentações</Title>
+
+            <List showsVerticalScrollIndicator={false}
+                data={historico}
+                keyExtractor={item => item.key}
+                renderItem={ ({ item }) => ( <HistoricoList data={item}/> )}
+            />
+        </Background>
     )
 }
