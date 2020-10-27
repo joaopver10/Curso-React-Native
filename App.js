782bef119cedbe885d58d9ed6197b271154cb5f8
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-10-27 19:23:10 -0300

    mapa com geolocalização

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView from 'react-native-maps'
 
 export default function App() {
+  const [mapa, setMapa] = useState(null)
+
+  useEffect(() => {
+    async function pegaLocal() {
+      await navigator.geolocation.getCurrentPosition(
+        async ({coords:{latitude, longitude} }) => {
+          setMapa({
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta:0.0922,
+            longitudeDelta: 0.0421
+          })
+        },
+         {
+           timout: 2000,
+           maximumAge: 1000,
+
+         }
+      )
+    }
+    pegaLocal()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Projeto mapas!</Text>
       <StatusBar style="auto" />
+      <MapView
+        style={{ width: 350, height: 350 }}
+        region={mapa}
+        showsUserLocation
+        loadingEnabled
+        minZoomLevel={18}
+        rotateEnabled={true}
+      />
+
+
     </View>
   );
 }
